2660a38907b24d5279260b99676f74fd03a044e3
avishk <129966113+seeavishk@users.noreply.github.com>
2023-06-05 22:48:18 +0530

    Add files via upload

---
+++ ch6__operatorsandexpressions.js
@@ -0,0 +1,19 @@
+console.log("operators in js")
+let a=55;
+let b=45;
+console.log("a+b =",a+b)
+console.log(a**b)
+console.log(a/b)
+console.log(a%b)
+//********assignment operators  */
+// a+=6
+a*=6;
+console.log(a)
+//the use of == is like it only matches up the value of the variable given
+//=== it will check both the values and the datatype for the given variables 
+k=8;
+j="8"
+console.log(k==j)
+console.log(k===j)
+
+
